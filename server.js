60667a1948fe12c53ca74fcab367ab4bb2f86718
Ryan Manns <w.r.manns@outlook.com>
2021-06-29 22:14:03 -0400

    update mongoose connection

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+//    useUnifiedTopology: true,
+//    useCreateIndex: true,
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/budget',
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: true
+    useFindAndModify: false
   }
 );
 
